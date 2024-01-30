153b04bfd41499f848d83852e5f8a14763f042c7
Idba1 <moniraislamself@gmail.com>
2024-01-30 20:28:14 +0600

    case.js

---
+++ case.js
@@ -0,0 +1,26 @@
+const subject = "Chemistry";
+const book = "chemistry";
+
+if(subject.toLowerCase() === book.toLowerCase()){
+    console.log("same");
+}
+else{
+    console.log("different");
+}
+
+
+
+
+
+
+
+
+const item = "  nine";
+const semester = "nine  ";
+
+if(item.trim() === semester.trim()){
+    console.log("same");
+}
+else{
+    console.log("different");
+}
