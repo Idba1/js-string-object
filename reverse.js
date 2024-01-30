dd91193fe429ae065c121fd1cac0a0e9cde3bb03
Idba1 <moniraislamself@gmail.com>
2024-01-31 04:16:09 +0600

    reverse.js

---
+++ reverse.js
@@ -0,0 +1,24 @@
+const sentence = "I am learning web Dev."
+let reverse = "";
+for(const letter of sentence){
+    reverse = letter + reverse;
+}
+console.log(reverse);
+
+
+
+
+
+
+
+let rev = "";
+for(let i=0; i<sentence.length; i++){
+    rev = sentence[i] + reverse;
+}
+console.log(rev);
+
+
+
+let reversed = "";
+reversed = sentence.split("").reverse().join("");
+console.log(reversed);
