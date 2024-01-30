e614cf188094359ca0c0d400ccb0ad616b32203b
Idba1 <moniraislamself@gmail.com>
2024-01-30 18:32:19 +0600

    string declear

---
+++ string.js
@@ -0,0 +1,8 @@
+const country = 'Bangladesh';
+const division = "Dhaka";
+const distric = `newkhalie`;
+const thana = new String ('Demra');
+
+
+console.log(country.length);
+console.log(distric[2]);
