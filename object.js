f951dacf23884dddeb42964801af7c7dc803e4b2
Idba1 <moniraislamself@gmail.com>
2024-01-31 04:42:00 +0600

    object.js

---
+++ object.js
@@ -0,0 +1,16 @@
+const subject = {
+    name : 'Computer Fundamental',
+    code : 'SE111',
+    teacher : 'Rashedul Sir',
+    teacherInitial : 'RHH',
+    examDate : '4 Dec',
+    chapter : {
+        chapter1 : 'Number System',
+        chapter2 : 'Boolean Algebra'
+    }, 
+    examDetails : {
+        term : 'Final',
+        marks : 100,
+    }
+}
+console.log(subject);
