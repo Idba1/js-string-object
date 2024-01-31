fc57f43c264f0803693c957f89f206976c09908c
Idba1 <moniraislamself@gmail.com>
2024-01-31 06:33:41 +0600

    declare.js

---
+++ declare.js
@@ -0,0 +1,13 @@
+const pen = { 
+    brand: 'econo', 
+    price: 10, 
+    color: 'black' 
+};
+console.log(pen);
+
+
+const pencil = new Object();
+console.log(pencil);
+
+const rubber = Object.create({});
+console.log(rubber);
